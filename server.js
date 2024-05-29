0dcd65eb4daccd806be4a0ac82315b0c790a3d08
gabrielpires11 <gabri4590@gmail.com>
2024-05-29 17:05:51 -0300

    atualizações no server.js

--- server.js
+++ server.js
@@ -31,6 +31,42 @@ if(err){
 console.log("Conectado com banco de dados!");
 });
 
+app.use(session({
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: true,
+    cookie: { secure: false}
+}));
+
+const authenticateSession = (req, res, next) =>{
+    if(!req.session.userId){
+        return res.status(401).send("Acesso negado, faça login para continuar")
+    }
+    next();
+}
+
+app.post('/login', (req, res) => {
+
+    const {cpf, senha} = req.body;
+
+    db.query ("SELECT * FROM usuarios WHERE cpf = ?", [cpf], 
+    async (err, results) => {
+        if(err) return res.status(500).send("Server com erro");
+if(results.length === 0) return res.status(500).send(
+    "CPF ou senha incorretos");
+
+    const usuario = results[0];
+    const senhaCorreta = await bcrypt.compare(
+        senha, usuario.senha);
+        if(!senhaCorreta) return res.status(500).send(
+            "CPF ou senha incorretos");
+            req.session.userId = usuario.idUsuarios;
+            console.log("idUsuarios: ", usuario.idUsuarios);
+            res.json({ message: "Login bem-sucedido"});
+    })
+})
+
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(
     `Servidor rodando na porta ${PORT}`))
+
